a15af7f686153452954d716f7a90686dc27ea86d
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-10 00:27:56 +0530

    issue fix

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,4 @@
-//'use client'
+'use client'
 export default function UserProfile({params}: any) {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
